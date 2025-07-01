4e154e2d7dd21ac1bf6167f78d646d2e1d9cafcb
Indhuja <indhujakanna2005@gmail.com>
2025-07-01 22:44:21 +0530

    fix:MongoStore error fixed

--- middlewares/expressSecret.js
+++ middlewares/expressSecret.js
@@ -1,9 +1,15 @@
 const express =require("express");
 const app=express();
 const session=require("express-session")
+const MongoStore = require("connect-mongo");
 app.use(session({
-  secret: process.env.SESSION_SECRET || "defaultSecretKey",
+  secret: process.env.SESSION_SECRET || "fallbackSecret",
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: false } // true only if using HTTPS
+  cookie: { secure: false }, // true only if using HTTPS
+  store: MongoStore.create({
+    mongoUrl: process.env.MONGODB_URI,
+    ttl: 14 * 24 * 60 * 60 // 14 days
+  })
+
 }));
